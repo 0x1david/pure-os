703e297d01d1a3b7f9fc7073bc11886a5d80c9f4
David Bousi <bousi.david@gmail.com>
2026-02-06 20:44:33 +0100

    Use VGA buffer for kernel output and panic handler
    Wire up the vga_buffer module, print a hello message on boot, and
    display panic info via println! instead of silently looping.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 #![no_std]
 #![no_main] // disable Rust-level entry points
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 /// This is the new entry point -> _start is the name linker looks for as a default
-#[unsafe(no_mangle)]
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello world!");
+    println!("Here comes numbers: `{}` `{}` !", 42, 1.337);
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
