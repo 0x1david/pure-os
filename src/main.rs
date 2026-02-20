4853b1dc9b1c699f615cda1ea0060b9cdcbaab08
David Bousi <bousi.david@gmail.com>
2026-02-20 15:31:49 +0100

    abstracted exceptions

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ pub extern "C" fn _start() -> ! {
     println!("Hello world!");
     println!("Here comes the numbers: `{}` `{}` !", 42, 1.337);
 
+    pure_os::init();
+
+    x86_64::instructions::interrupts::int3(); // TODO: Replace with custom IDT
+
     #[cfg(test)]
     test_main();
 
